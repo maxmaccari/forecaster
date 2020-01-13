67be04c2cb8dee6233268af3eaa867bfd00ae24d
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-01-13 00:30:36 -0400

    Create show route

--- app/router.js
+++ app/router.js
@@ -8,4 +8,8 @@ export default class Router extends EmberRouter {
 
 Router.map(function() {
   this.route('credits');
+
+  this.route('forecast', function() {
+    this.route('show', { path: '/:city_name' });
+  });
 });
