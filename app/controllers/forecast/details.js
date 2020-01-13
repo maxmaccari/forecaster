691082311437dc7a754f70ab3080edfee12c2d52
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-01-13 17:53:25 -0400

    Implement forecast details data fetching

--- app/controllers/forecast/details.js
+++ app/controllers/forecast/details.js
@@ -1,10 +1,37 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import moment from 'moment';
 
 export default class ForecastDetailsController extends Controller {
   @service router;
 
   get cityName() {
     return this.router.currentRoute.params.city;
+  }
+
+  get dateParam() {
+    return this.router.currentRoute.params.date;
+  }
+
+  get currentDate() {
+    return moment(this.dateParam).format('dddd D');
+  }
+
+  get weather() {
+    const bestWeatherIndex = this.model.list.findIndex((weather) => {
+      return weather.dt_txt.includes(`${this.dateParam} 12:00:00`);
+    });
+
+    if (bestWeatherIndex === -1) {
+      return this.model.list[0];
+    }
+
+    return this.model.list[bestWeatherIndex];
+  }
+
+  get todayForecast() {
+    return this.model.list.filter((weather) => {
+      return weather.dt_txt.substr(0, 10) === this.dateParam;
+    });
   }
 }
--- app/routes/forecast/details.js
+++ app/routes/forecast/details.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class ForecastDetailsRoute extends Route {}
+
+export default class ForecastDetailsRoute extends Route {
+  @service openWeatherMap;
+
+  model(params) {
+    return this.openWeatherMap.getForecastFrom(params.city);
+  }
+}
