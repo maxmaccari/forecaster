5c6c0bd3f9b285380dee04ec41d9bb348c99cf64
Maxsuel Maccari <maxmaccari@hotmail.com>
2020-01-13 07:17:57 -0400

    Load forecast together weather

--- app/controllers/forecast/show.js
+++ app/controllers/forecast/show.js
@@ -2,7 +2,8 @@ import Controller from '@ember/controller';
 
 export default class ForecastShowController extends Controller {
   get weather() {
-    if (this.model.weather[0]) return this.model.weather[0];
+    if (this.model.currentWeather.weather[0])
+      return this.model.currentWeather.weather[0];
 
     return null;
   }
--- app/routes/forecast/show.js
+++ app/routes/forecast/show.js
@@ -1,10 +1,14 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class ForecastShowRoute extends Route {
   @service openWeatherMap;
 
   model(params) {
-    return this.openWeatherMap.getCurrentWeatherFrom(params.city);
+    return hash({
+      currentWeather: this.openWeatherMap.getCurrentWeatherFrom(params.city),
+      forecast: this.openWeatherMap.getForecastFrom(params.city),
+    });
   }
 }
